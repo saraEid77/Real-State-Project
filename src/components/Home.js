b0bf099d770f1312393e44777060d85bc0230499
Sara Eid <sara.eid.77777@gmail.com>
2020-09-08 16:19:05 +0200

    test

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,13 +12,13 @@ const home = () => {
     return (
         <div>
             
-            <Section1 />  
-            <Section2 />
+            {/* <Section1 />  
+            <Section2 /> */}
             {/* <Section3 />   */}
-            <Section7 />
+            {/* <Section7 />
             <Section4 />
             <Section5 />     
-            <Footer />
+            <Footer /> */}
             
             
             
